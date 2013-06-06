4680aa33ffd3a1e929dc6b0977bc71c23bf13be9
jasonmit <jason.mitchell.w@gmail.com>
2013-06-06 13:41:15 -0400

    Source cleaned up and added jQuery checking

--- main.js
+++ main.js
@@ -1,72 +1,80 @@
-;(function ($, win, doc) {
-	$.fn.landmark = function (opts) {
-		var settings = $.extend({ 'id' : 'landmarkjs' }, opts),
-			isDrawn = false,
-			anchors = [], 
-			onScroll,
-			el,
-			$el,
-			innerText;
-		
-		if(opts) {
-			if(typeof opts.id === 'undefined' && document.getElementById(settings.id)) {
-				console.error("!! landmark.js -> was already rendered on this object");
-				return false;
-			}
-			else if(opts.id && !doc.getElementById(settings.id)) {
-				console.error("!! landmark.js -> landmark element id ", settings.id, " does not exist");
-				return false;
-			}
-			else if(typeof opts.id !== 'undefined') {
-				isDrawn = true;
-			}
-		}
-
-		if(!isDrawn) {
-			el = doc.createElement('div');
-			el.setAttribute('id', settings.id);
-			el.className = 'landmark-tip';
-			el.style.display = 'none';
-			doc.getElementsByTagName('body')[0].appendChild(el);
-		} 
-		else {			
-			el = doc.getElementById(settings.id);
-		}
-
-		$el = $(el); // cache it for later
-
-		this.each(function () {
-			var $this = $(this);
+if('undefined' === typeof jQuery) {	
+	console.warn('!! landmark.js -> jQuery dependency is missing');
+}
+else {
+	;(function ($, win, doc) {
+		$.fn.landmark = function (opts) {
+			var settings = $.extend({ 'id' : 'landmarkjs' }, opts),
+				isDrawn = false,
+				anchors = [], 
+				$el;
 			
-			anchors.push({ 
-				id: $this.attr('id'), 
-				y: $this[0].offsetTop + $this[0].offsetHeight,
-				$el: $this 
-			});
-		});
-
-		onScroll = function (e) {
-			var docEl = doc.documentElement, 
-				body = doc.body,
-				top = (docEl && docEl.scrollTop  || body && body.scrollTop  || 0),
-				closest = {};
-
-			anchors.forEach(function (o) {
-				o.diff = Math.abs(o.y - top);
-
-				if(typeof closest.diff === 'undefined' || o.diff < closest.diff && top > o.y) {
-					closest = o;
+			if(opts) {
+				if('undefined' === typeof opts.id && document.getElementById(settings.id)) {
+					console.error("!! landmark.js -> was already rendered on this object");
+					return false;
 				}
-			});
- 			
-			$el.text(closest.$el.data('anchor'));
-			
-			if(!isDrawn) {
-				el.style.display = 'block';
-				isDrawn = true;
+				else if(opts.id && !doc.getElementById(settings.id)) {
+					console.error("!! landmark.js -> landmark element id ", settings.id, " does not exist");
+					return false;
+				}
+				else if(typeof opts.id !== 'undefined') {
+					isDrawn = true;
+				}
 			}
-		};
 
-		win.onscroll = onScroll; /* todo: this can be debounced */
-	};
-})(jQuery, window, document);
+			// self-executing function that intializes $el
+			(function() {
+				var el;
+
+				if(!isDrawn) {
+					el = doc.createElement('div');
+					el.setAttribute('id', settings.id);
+					el.className = 'landmark-tip';
+					el.style.display = 'none';
+					doc.getElementsByTagName('body')[0].appendChild(el);
+				} 
+				else {			
+					el = doc.getElementById(settings.id);
+				}
+
+				$el = $(el); // cache it for later
+			})();
+
+			this.each(function () {
+				var $this = $(this);
+				
+				anchors.push({ 
+					id: $this.attr('id'), 
+					y: $this[0].offsetTop + $this[0].offsetHeight,
+					$anchor: $this 
+				});
+			});
+
+			win.onscroll = function (/* evt */) {
+				var docEl = doc.documentElement, 
+					body = doc.body,
+					top = (docEl && docEl.scrollTop  || body && body.scrollTop  || 0),
+					closest = {};
+
+				// determines which of the landmarks is closest to the top and out of view
+				for(var i = 0, len = anchors.length; i < len; i++) {
+					var o = anchors[i];
+					o.diff = Math.abs(o.y - top);
+
+					if('undefined' === typeof closest.diff || o.diff < closest.diff 
+						&& top > o.y) { 
+							closest = o; 
+						}
+				}
+	 			
+				$el.text(closest.$anchor.data('anchor'));
+				
+				if(!isDrawn) {				
+					$el.css('display', 'block');
+					isDrawn = true;
+				}
+			};
+		};
+	})(jQuery, window, document);
+}
